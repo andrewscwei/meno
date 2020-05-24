74f6699d813617e23f569353827b1ab0298df264
andrewscwei <andrewscwei@users.noreply.github.com>
2020-05-24 19:04:38 +0000

    [SKIP CI] Update CI/CD workflows

--- src/vdom/index.js
+++
@@ -1,4 +0,0 @@
-export { default as createElement } from './createElement';
-export { default as createVTree } from './createVTree';
-export { default as patch } from './patch';
-export { default as vnode } from './vnode';
--- tests/index.js
+++
@@ -1,9 +0,0 @@
-// © Andrew Wei
-
-import { describe, it } from 'mocha';
-
-describe('meno', () => {
-  it('should have some tests soon', function() {
-
-  });
-});
